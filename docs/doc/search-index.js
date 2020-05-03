44404eb3d6daf96f85544fa8b2526a4fb53709c4
Shawn Hice <shawn.hice@autodesk.com>
2020-05-03 16:05:13 -0700

    Removed reference to old crate

--- docs/doc/search-index.js
+++ docs/doc/search-index.js
@@ -1,5 +1,4 @@
 var N=null,E="",T="t",U="u",searchIndex={};
 
-searchIndex["RustConsoleEmulator"]={"doc":E,"i":[],"p":[]};
 searchIndex["rust_console_emulator"]={"doc":E,"i":[],"p":[]};
 initSearch(searchIndex);addSearchOptions(searchIndex);
