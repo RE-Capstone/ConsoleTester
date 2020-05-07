5b1e0717845c6b8564900010e85e06d99be2be82
davmeyerpdx <davmeyer@pdx.edu>
2020-05-07 02:07:41 -0700

    Testing documentation

--- src/docs.rs
+++ src/docs.rs
@@ -1,3 +1,17 @@
-fn functionExample () {
-    println!("Test Function!");
+/// Function: addOne
+/// 
+/// Adds one to an integer.
+/// 
+/// Parameters: 
+///     x - i32
+/// Returns:
+///     i32
+/// 
+/// # Example
+/// 
+/// ```
+/// addOne(3);
+/// ```
+pub fn addOne (x: i32) -> i32 {
+    x + 1
 }
