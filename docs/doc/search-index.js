285a2dcfbdad463f9c2e5a0b9ca4dba3c8da5e6f
Shawn Hice <shawn.hice@autodesk.com>
2020-06-05 14:40:34 -0700

    Updated the git doc generator action to be correct

--- docs/doc/search-index.js
+++ docs/doc/search-index.js
@@ -1,3 +1,4 @@
-var searchIndex={};
-searchIndex["rust_console_emulator"] = {"doc":"","i":[],"p":[]};
-addSearchOptions(searchIndex);initSearch(searchIndex);
+var N=null,E="",T="t",U="u",searchIndex={};
+
+searchIndex["rust_console_emulator"]={"doc":E,"i":[[5,"main","rust_console_emulator",E,N,[[]]]],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
