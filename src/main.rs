fced6d5ae05477bf6b1b1377bb4b324a3b61cc3e
Grady Ku <gradku@pdx.edu>
2020-06-22 13:16:15 -0700

    Added my code to buffer.rs
    also slightly modified main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::{self, Write};
+use std::io::prelude::*;
+use std::fs::File;
+
+use console_tester::buffer::TermWriter;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!");
 }
