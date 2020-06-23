480adc9b7badf5c129a68bf3624d703a87c834e3
Grady Ku <gradku@pdx.edu>
2020-06-22 18:34:19 -0700

    Added unit tests to main.rs
    unit tests for testing functionality of TermWriter struct

--- src/main.rs
+++ src/main.rs
@@ -18,3 +18,47 @@ fn main() {
     let byte_literal = b"write_all()";
     buffer.write_all(byte_literal);
 }
+
+#[cfg(test)] //for 'cargo test'
+mod tests {
+    use super::*;
+
+    #[test]
+    fn vec_string_bytes() {
+        let vec1 = vec!["Some".to_string(), "junk".to_string(), "text".to_string()];
+        let joined_vec1 = vec1.join(" ");
+        let vec1_bytes = joined_vec1.as_bytes();
+
+        let bytes_literal = b"Some junk text";
+
+        assert_eq!(vec1_bytes, bytes_literal);
+    }
+
+    #[test]
+    fn termwriter_write() {
+        let bytes_literal = b"Some junk text";
+
+        let mut buffer = TermWriter::new();
+        let bytes_written = buffer.write(bytes_literal);
+
+        assert_eq!(bytes_written.unrap(), 14);
+    }
+
+    #[test]
+    fn termwriter_write_all() {
+        let bytes_literal = b"Some junk text";
+
+        let mut buffer = TermWriter::new();
+
+        assert_eq!((), buffer.write_all(byte_literal).unwrap()); //"no news is good news"
+    }
+
+    #[test]
+    fn termwriter_flush() {
+        let bytes_literal = b"Some junk text";
+
+        let mut buffer = TermWriter::new();
+        let bytes_written = buffer.write(bytes_literal);
+        assert_eq!((), buffer.flush().unwrap())
+    }
+}
