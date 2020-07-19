bb89a8c695c409e6285559bbbf2e0a89445a08f5
Shawn Hice <shawnhice@gmail.com>
2020-07-19 09:46:56 -0700

    Updated Docs - Removed Action to generate docs pending further development

--- docs/doc/implementors/core/marker/trait.Send.js
+++ docs/doc/implementors/core/marker/trait.Send.js
@@ -1,3 +1,9 @@
 (function() {var implementors = {};
-implementors["console_tester"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"console_tester/buffer/struct.TermWriter.html\" title=\"struct console_tester::buffer::TermWriter\">TermWriter</a>","synthetic":true,"types":["console_tester::buffer::TermWriter"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"console_tester/term/struct.TermStrings.html\" title=\"struct console_tester::term::TermStrings\">TermStrings</a>","synthetic":true,"types":["console_tester::term::TermStrings"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+implementors["console_tester"] = [{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"console_tester/buffer/struct.TermWriter.html\" title=\"struct console_tester::buffer::TermWriter\">TermWriter</a>",synthetic:true,types:["console_tester::buffer::TermWriter"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"console_tester/term/struct.TermStrings.html\" title=\"struct console_tester::term::TermStrings\">TermStrings</a>",synthetic:true,types:["console_tester::term::TermStrings"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
